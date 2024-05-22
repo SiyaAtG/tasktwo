44f90d73960d914611dc9272d513bc5e4b43f4b4
Siyabulela <g20g9176@campus.ru.ac.za>
2024-05-22 22:16:13 +0200

    3- output to user selected fields in JSON format

--- XMLReader.java
+++ XMLReader.java
@@ -1,52 +1,66 @@
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class XMLReader {
     public static void main(String[] args) {
         try {
-            // Load the XML file
             File xmlFile = new File("input.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(xmlFile);
             doc.getDocumentElement().normalize();
-            
-            // Prompt the user to specify which fields they want to view
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("Which fields do you want to view? (name, postalZip, region, country, address, list)");
-            // Read the user input and split it into an array of selected fields
             String[] selectedFields = scanner.nextLine().split("\\s*,\\s*");
-            
-            // Get the list of record elements from the XML document
+
             NodeList recordList = doc.getElementsByTagName("record");
-            // Iterate over each record element
+
+            System.out.println("["); // Start of JSON array
+
             for (int i = 0; i < recordList.getLength(); i++) {
                 Node recordNode = recordList.item(i);
                 if (recordNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element recordElement = (Element) recordNode;
-                    System.out.println("Record " + (i+1) + ":");
-                    
-                    // Iterate over each selected field
-                    for (String field : selectedFields) {
-                        // Get the list of elements with the current field name
+
+                    // Start of JSON object for each record
+                    System.out.println("  {");
+
+                    for (int j = 0; j < selectedFields.length; j++) {
+                        String field = selectedFields[j];
                         NodeList fieldList = recordElement.getElementsByTagName(field);
-                        // If the field exists in the current record
                         if (fieldList.getLength() > 0) {
-                            // Get the first occurrence of the field
                             Node fieldNode = fieldList.item(0);
-                            // Print the field name and its value
-                            System.out.println(field + ": " + fieldNode.getTextContent());
+                            String fieldValue = fieldNode.getTextContent();
+                            // Output field name and value as JSON key-value pair
+                            System.out.println("    \"" + field + "\": \"" + fieldValue + "\"");
+                            // Add comma if not the last field
+                            if (j < selectedFields.length - 1) {
+                                System.out.println(",");
+                            }
                         } else {
-                            // If the field doesn't exist in the current record, print "Not found"
-                            System.out.println(field + ": Not found");
+                            // Output field name with "Not found" value as JSON key-value pair
+                            System.out.println("    \"" + field + "\": \"Not found\"");
+                            if (j < selectedFields.length - 1) {
+                                System.out.println(",");
+                            }
                         }
                     }
-                    // Separate records with a line
-                    System.out.println("---------------------------------------------");
+
+                    // End of JSON object for each record
+                    System.out.println("  }");
+
+                    // Add comma if not the last record
+                    if (i < recordList.getLength() - 1) {
+                        System.out.println(",");
+                    }
                 }
             }
+
+            System.out.println("]"); // End of JSON array
+
         } catch (Exception e) {
             e.printStackTrace();
         }
